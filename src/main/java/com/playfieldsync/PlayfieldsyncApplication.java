c4fe5177efe015cd16dc62cd8b55c30485af02f4
Rafael Toresani <rtoresani@gmail.com>
2024-03-26 13:54:17 -0300

    first commit

---
+++ src/main/java/com/playfieldsync/PlayfieldsyncApplication.java
@@ -0,0 +1,13 @@
+package com.playfieldsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfieldsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfieldsyncApplication.class, args);
+	}
+
+}
